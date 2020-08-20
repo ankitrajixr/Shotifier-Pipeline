4afa7cdf96a4e3d70ce0f7fb2c3cc607ee2a9576
Sindhu <60362151+Sindhu-Subramanya@users.noreply.github.com>
2020-08-20 10:11:27 +0200

    Add files via upload

---
+++ task_4.py
@@ -0,0 +1,33 @@
+#############################################################################################################################################################
+#This code filters the events and sub events that are responsible for a goal
+#############################################################################################################################################################
+
+#Importing libraries
+import pymongo 
+import pprint
+import pandas as pd
+
+#connecting mongodb and python
+mongo_uri = "mongodb://localhost:27017/"  
+client = pymongo.MongoClient(mongo_uri)
+client.list_database_names()
+db = client.Wyscout
+db.list_collection_names()
+
+
+#Filtering events and sub events with respect to goal
+table=db.matches_events_v02.find({"Tag_1":"101"},{"_id":0, "eventName":1, "subEventName":1,  "playerId":1, "teamId":1, "matchId":1 })
+table_df=pd.DataFrame(table2)
+table_df.head(100)
+
+
+#Filtering tags with respect to shots
+table1= db.matches_events_v02.aggregate([
+{'$project':{"_id":0, "eventName":1, "subEventName":1, "playerId":1, "teamId":1, "matchId":1}
+},
+{'$match':{'$or':[{"eventName":"Shot"},{"subEventName":"Shot"}]}}
+])
+table1_df =pd.DataFrame(table)
+table1_df.head(1000)
+len(table1_df)
+
